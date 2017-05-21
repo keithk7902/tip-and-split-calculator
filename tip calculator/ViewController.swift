e8c4625cf2cd257199196383bdf19928989f4a69
keithk7902 <keithk7902@gmail.com>
2017-05-21 22:37:14 +0800

    v1

--- tip calculator/ViewController.swift
+++ tip calculator/ViewController.swift
@@ -19,7 +19,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBOutlet weak var bill: UITextField!
+    @IBOutlet weak var tip: UITextField!
+    @IBOutlet weak var total: UILabel!
+    @IBOutlet weak var numberOfPeople: UITextField!
+    @IBOutlet weak var each: UILabel!
+    @IBAction func calculate(_ sender: UIButton) {
+        let sum:Double = round(Double(bill.text!)!*(1+Double(tip.text!)!)*100)/100
+        let eachPays:Double = round(sum/Double(numberOfPeople.text!)!*100)/100
+        total.text = "$"+String(sum)
+        each.text = "$"+String(eachPays)
+    }
 }
 
